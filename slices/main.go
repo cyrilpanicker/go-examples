e4575278f1a78b1ff94a6f329bf4d4b0d87607b2
cyril panicker <cyril.panicker@walmart.com>
2019-10-06 02:21:50 +0530

    slices

--- slices/main.go
+++ slices/main.go
@@ -7,6 +7,10 @@ type point struct {
 	y int
 }
 
+func describeSlice(name string, x []int) {
+	fmt.Println(name, x, len(x), cap(x), x == nil)
+}
+
 func main() {
 
 	array := [6]int{0, 1, 2, 3, 4, 5}
@@ -33,10 +37,66 @@ func main() {
 	slice5 := []struct {
 		a int
 		b bool
-	}{
-		{1, false},
-		{2, true},
-	}
+	}{{1, false}, {2, true}}
 	fmt.Println("slice5", slice5)
+
+	slice6 := array[:0]
+	describeSlice("slice6", slice6)
+
+	//higher limit of a slice can be extended upto its "capacity".
+	//changing the higher limit of a slice does not change its "capacity"
+	slice6 = slice6[:4]
+	describeSlice("slice6", slice6)
+
+	//changing the lower limit of a slice "changes" its "capacity"
+	slice6 = slice6[2:]
+	describeSlice("slice6", slice6)
+
+	var slice7 []int
+	describeSlice("slice7", slice7)
+
+	slice8 := make([]int, 5)
+	describeSlice("slice8", slice8)
+
+	slice9 := make([]int, 0, 5)
+	describeSlice("slice9", slice9)
+	slice9 = slice9[:cap(slice9)]
+	describeSlice("slice9", slice9)
+	slice9 = slice9[1:]
+	describeSlice("slice9", slice9)
+	slice9 = slice9[:0]
+	describeSlice("slice9", slice9)
+	slice9 = slice9[:2]
+	describeSlice("slice9", slice9)
+	slice9 = slice9[2:4]
+	describeSlice("slice9", slice9)
+
+	sliceOfSlicesOfStrings := [][]string{
+		[]string{"a", "b"},
+		[]string{"c", "d"},
+	}
+	fmt.Println(sliceOfSlicesOfStrings)
+
+	var slice10 []int
+	describeSlice("slice10", slice10)
+	slice10 = append(slice10, 0, 2)
+	describeSlice("slice10", slice10)
+	slice10 = append(slice10, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+	describeSlice("slice10", slice10)
+
+	for index, value := range slice10 {
+		fmt.Printf("%d-%d, ", index, value)
+	}
+	fmt.Println()
+
+	for _, value := range slice10 {
+		fmt.Printf("%d, ", value)
+	}
+	fmt.Println()
+
+	for index := range slice10 {
+		fmt.Printf("%d, ", index)
+	}
+	fmt.Println()
 
 }
